2ddc41c8877036a22e31c4298d4e4477d1d5ea60
Eric Brooks <57463763+himynameisericbrooks@users.noreply.github.com>
2019-11-19 03:26:34 -0600

    myfolio

---
+++ img/CSS Layout - The position Property_files/snhbMLSettings.js
@@ -0,0 +1,30 @@
+snhb.modules.machineLearning.mlSettings = {
+	right_bottom_medium_rectangle: {
+		mean: 0.050000,
+		dev: 0.055000
+	},
+	wide_skyscraper: {
+		mean: 0.050000,
+		dev: 0.055000
+	},
+	main_leaderboard: {
+		mean: 0.050000,
+		dev: 0.055000
+	},
+	bottom_medium_rectangle: {
+		mean: 0.061965,
+		dev: 0.015000
+	},
+	try_it_leaderboard: {
+		mean: 0.050000,
+		dev: 0.055000
+	},
+	mid_content: {
+		mean: 0.050000,
+		dev: 0.055000
+	},
+	sidebar_sticky: {
+		mean: 0.050000,
+		dev: 0.055000
+	},
+};
